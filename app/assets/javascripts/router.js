5b3a00d413b3b37715e59f59c4747c95a0321d3e
Maureen Dugan & vincent rotter <student@epicodus.com>
2014-04-28 11:58:43 -0700

    nest newTask in index

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 ToDo.Router.map(function() {
-  this.resource('tasks', { path: '/' });
-  this.resource('new_task', { path: '/tasks/new'});
+  this.resource('tasks', { path: '/' }, function() {
+    this.resource('new_task', { path: '/tasks/new'});
+  });
   this.resource('task', { path: '/tasks/:id'});
   this.resource('edit_task', { path: '/tasks/:id/edit'});
 });
