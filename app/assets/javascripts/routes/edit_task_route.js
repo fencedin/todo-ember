2890c574c569277fb650c90028239e69c106de34
Maureen Dugan & vincent rotter <student@epicodus.com>
2014-04-28 12:31:04 -0700

    add deactivate and confirmation when navigating away from route

--- app/assets/javascripts/routes/edit_task_route.js
+++ app/assets/javascripts/routes/edit_task_route.js
@@ -1,5 +1,23 @@
 ToDo.EditTaskRoute = Ember.Route.extend({
+
   model: function(params) {
     return this.store.find('task', params.id);
+  },
+
+  deactivate: function() {
+    var model = this.get('controller.model');
+    model.rollback();
+    if (model.get('isNew')) {
+      model.deleteRecord();
+    }
+  },
+
+  actions: {
+    willTransition: function(transition) {
+      var model = this.get('controller.model');
+      if (model.get('isDirty') && !confirm('You have unsaved changes. They will be lost if you continue!')) {
+        transition.abort();
+      }
+    }
   }
 });
--- app/assets/javascripts/routes/new_task_route.js
+++ app/assets/javascripts/routes/new_task_route.js
@@ -1,5 +1,23 @@
 ToDo.NewTaskRoute = Ember.Route.extend({
+
   model: function() {
     return this.store.createRecord('task');
+  },
+
+  deactivate: function() {
+    var model = this.get('controller.model');
+    model.rollback();
+    if (model.get('isNew')) {
+      model.deleteRecord();
+    }
+  },
+
+  actions: {
+    willTransition: function(transition) {
+      var model = this.get('controller.model');
+      if (model.get('isDirty') && !confirm('You have unsaved changes. They will be lost if you continue!')) {
+        transition.abort();
+      }
+    }
   }
 });
