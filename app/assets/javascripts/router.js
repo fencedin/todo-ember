7094bad44ef734e35f59f62c9c5151132247abb1
Maureen Dugan & vincent rotter <student@epicodus.com>
2014-04-28 12:12:04 -0700

    stylize buttons

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,7 +1,8 @@
 ToDo.Router.map(function() {
   this.resource('tasks', { path: '/' }, function() {
     this.resource('new_task', { path: '/tasks/new'});
+    this.resource('task', { path: '/tasks/:id'}, function() {
+        this.resource('edit_task', { path: '/tasks/:id/edit'});
+    });
   });
-  this.resource('task', { path: '/tasks/:id'});
-  this.resource('edit_task', { path: '/tasks/:id/edit'});
 });
